725fe8aff2e8b02db7f28aedd79ee4993ad23a9e
Kalachev Gleb <gleb.kalachev@yandex.ru>
2026-03-20 19:22:25 +0300

    chore: Update footer and application name in settings

--- services/web/config/settings.defaults.js
+++ services/web/config/settings.defaults.js
@@ -762,7 +762,7 @@ module.exports = {
     stepTimeout: parseInt(process.env.SMOKE_TEST_STEP_TIMEOUT || '10000', 10),
   },
 
-  appName: process.env.APP_NAME || 'Overleaf (Community Edition)',
+  appName: process.env.APP_NAME || 'LaTeX Space',
 
   adminEmail: process.env.ADMIN_EMAIL || 'placeholder@example.com',
   adminDomains: process.env.ADMIN_DOMAINS
@@ -770,15 +770,15 @@ module.exports = {
     : undefined,
 
   nav: {
-    title: process.env.APP_NAME || 'Overleaf Community Edition',
+    title: process.env.APP_NAME || 'LaTeX Space',
 
     hide_powered_by: process.env.NAV_HIDE_POWERED_BY === 'true',
     left_footer: [],
 
     right_footer: [
       {
-        text: "<i class='fa fa-github-square'></i> Fork on GitHub!",
-        url: 'https://github.com/overleaf/overleaf',
+        text: "Fork on GitHub!",
+        url: 'https://github.com/KalachevGleb/latex-space',
       },
     ],
 
